c333e3314e57b5dd4dbf8fa74fcccfff37316f40
Shaikh Parwez Hasim <shaikhparwez963@gmail.com>
2024-08-22 21:24:40 +0530

    disclaimer update

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const Disclaimer = () => {
-  const [showDisclaimer, setShowDisclaimer] = React.useState(false);
+  const [showDisclaimer, setShowDisclaimer] = React.useState(true);
 
-  React.useEffect(() => {
-    if (hasCookie("disclaimer")) {
-      setShowDisclaimer(false);
-    } else {
-      setShowDisclaimer(true);
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   if (hasCookie("disclaimer")) {
+  //     setShowDisclaimer(false);
+  //   } else {
+  //     setShowDisclaimer(true);
+  //   }
+  // }, []);
 
   const handleAccept = () => {
     const expirationDate = new Date();
@@ -27,8 +27,8 @@ const Disclaimer = () => {
 
   return (
     <div className="fixed inset-0 bg-slate-700 bg-opacity-70 z-[5000] flex items-center justify-center py-10">
-      <div className="bg-white p-6 rounded shadow-lg flex flex-col gap-10 mx-5 lg:mx-40 max-sm:h-vh max-sm:w-vw overflow-auto">
-        <p className="flex justify-center text-2xl text-[#1f3864]">
+      <div className="bg-white p-6 rounded-xl shadow-lg flex flex-col gap-10 mx-5 lg:mx-40 max-sm:h-auto max-sm:gap-2 max-sm:w-vw overflow-auto">
+        <p className="custom-font font-semibold flex justify-center text-2xl text-[#1f3864]">
           Disclaimer
         </p>
         <div className="text-dark text-base mx-auto px-10 flex flex-col gap-5">
@@ -44,15 +44,20 @@ const Disclaimer = () => {
             placed by the user or any other third party on the information
             contained therein
           </p>
-          <button
-            className="bg-blue-400 py-2 px-10 rounded text-white hover:bg-blue-600 transition duration-300"
-            onClick={handleAccept}
-          >
-            Agree
-          </button>
-          <button onClick={() => redirect("https://www.google.com")}>
-            Don't Agree
-          </button>
+          <div className="flex flex-col lg:flex-row gap-2 mx-auto">
+            <button
+              className="bg-blue-400 py-2 px-10 rounded text-white hover:bg-blue-600 transition duration-300"
+              onClick={handleAccept}
+            >
+              I Accept
+            </button>
+            <button
+              className="bg-slate-500 py-2 text-white px-10 rounded hover:bg-red-400 transition duration-300"
+              onClick={() => redirect("https://www.google.com")}
+            >
+              I Reject
+            </button>
+          </div>
         </div>
       </div>
     </div>
